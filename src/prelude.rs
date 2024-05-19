a2d66ba308c68ebab10664dd542bacf2bce55155
1Kill2Steal <51541614+1Kill2Steal@users.noreply.github.com>
2024-05-19 22:41:31 +0300

    Added prelude

---
+++ src/prelude.rs
@@ -0,0 +1,10 @@
+#![allow(unused_imports)]
+
+#[doc(hidden)]
+pub use clap::Parser;
+
+pub use crate::enums::MatrixOperation;
+pub use crate::error::*;
+pub use crate::structs::Args;
+pub use crate::types::{Matrix, MatrixRef};
+pub use crate::utils::{matrix, matrix_operation, matrix_operation_unchecked};
