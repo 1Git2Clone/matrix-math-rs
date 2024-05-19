e6c0822e7f28581c6787e78a7f055765ff2dc605
1Kill2Steal <51541614+1Kill2Steal@users.noreply.github.com>
2024-05-19 23:57:23 +0300

    Updated doc examples to match the new generic impl

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,11 @@
 //! use matrix::prelude::*;
 //!
 //! fn interactive_matrices_example() -> Result<(), Error> {
-//!     let matrix1 = matrix(1, None, None);
+//!     let matrix1 = matrix::<i32>(1, None, None);
 //!
 //!     // By calling matrix2 with the length parameters of matrix1 like so. Doing the operations
 //!     // unchecked is **SAFE**.
-//!     let matrix2 = matrix(2, Some(matrix1.len()), Some(matrix1[0].len()));
+//!     let matrix2 = matrix::<i32>(2, Some(matrix1.len()), Some(matrix1[0].len()));
 //!
 //!     let sum = matrix_operation_unchecked(MatrixOperation::Addition, &matrix1, &matrix2);
 //!     println!("Sum:\n{:#?}", sum);
